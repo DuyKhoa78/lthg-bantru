326bab1f9ea076dbb5e501371675ed092b827aab
Duy Khoa <khoa18731@gmail.com>
2026-05-05 16:10:26 +0700

    Switch to free GitHub Pages domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',           // custom domain → dùng '/'
+  base: '/lthg-bantru/',           // đổi thành thư mục github pages
   build: {
     outDir: 'dist',
   },
